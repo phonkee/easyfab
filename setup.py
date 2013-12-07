b201ac65e11a6947e15c14a43dfb91ddcfe03193
phonkee <phonkee@phonkee.eu>
2013-12-07 16:37:02 +0100

    setup.py improvements

--- setup.py
+++ setup.py
@@ -1,24 +1,36 @@
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('LICENSE') as f:
+    license = f.read()
+
+with open('README.rst') as f:
+    readme = f.read()
+
+requires = [
+        'jinja2',
+        'fabric',
+        'unipath',
+]
 
 setup(
     name='easyfab',
-    version='0.3.6',
-    description='simpler deployments with fabric',
+    version='0.3.8',
+    description='Simple deployments with fabric',
+    long_description=readme,
     url='https://github.com/phonkee/easyfab/',
     author='phonkee',
     author_email='phonkee@phonkee.eu',
-    license='MIT',
+    license=license,
     packages=[
         'easyfab',
     ],
     scripts=[
         'bin/easyfab'
     ],
-    install_requires=[
-        'jinja2',
-        'fabric',
-        'unipath',
-    ],
+    install_requires=requires,
     package_data={
         'easyfab': [
             '*.conf',
@@ -27,5 +39,14 @@ setup(
             'templates/deployment/conf/*.*',
         ]
     },
-    zip_safe=False
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
 )
